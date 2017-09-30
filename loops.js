70d5c964ee76f58bc27cd1f805c1e5a6e8f7aa83
youngky hermanto <yhermanto@gmail.com>
2017-09-30 06:51:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ for (i=0; i < 25; i++){
     array.push("I am 1 strange loop.")
   }
   else {
-    array.push("I am ${i} strange loops.")
+    array.push('I am ${i} strange loops.'')
   }
 }
 return array
